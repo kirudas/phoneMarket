add04d6d9bcaacc1d45020098a017bf2885086ef
vilalta <253joanvilalta@gmail.com>
2018-02-22 20:09:37 +0100

    main a mitges

--- app/src/main/java/info/androidhive/recyclerview/MainActivity.java
+++ app/src/main/java/info/androidhive/recyclerview/MainActivity.java
@@ -1,6 +1,8 @@
 package info.androidhive.recyclerview;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DefaultItemAnimator;
@@ -11,52 +13,73 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private List<Mobile> mobileList = new ArrayList<>();
+    /*private List<Mobile> mobileList = new ArrayList<>();
     private RecyclerView recyclerView;
-    private MobilesAdapter mAdapter;
+    private MobilesAdapter mAdapter;*/
+    final static int ADD_Mobile = 1;
+
+    private Cursor Mobiles;
+    private MobilesAdapter adapter;
+    private MobilesSqlLiteHelper titHelper;
+    private MobilesConversor MobilesConv;
+    private RecyclerView llista;
+    private TextView lblNoData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        // recuperar els controls
+        lblNoData = (TextView) findViewById(R.id.lblNoData);
+        llista = (RecyclerView) findViewById(R.id.recycler_view);
 
-        recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        // vincular el menú contextual a la llista
+        registerForContextMenu(llista);
+
+        // crear l'objecte que crea la connexió amb la BD
+        titHelper = new  MobilesSqlLiteHelper(this, "Titulars.db", null, 2);
+        // obtenir l'objecte BD 
+        SQLiteDatabase db = titHelper.getWritableDatabase();
+        MobilesConv = new MobilesConversor(titHelper);
+
+        // Si s'ha obert correctament la BD
+        if(db != null) {
+            // actualitzar la llista
+            refreshData();
+            // Tancar la base de dades
+            db.close();
+        }      
+        /*recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
 
         mAdapter = new MobilesAdapter(mobileList);
 
         recyclerView.setHasFixedSize(true);
 
-        // vertical RecyclerView
-        // keep mobile_list_row.xmll width to `match_parent`
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext());
-
-        // horizontal RecyclerView
-        // keep mobile_list_row.xmll width to `wrap_content`
-        // RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.HORIZONTAL, false);
 
         recyclerView.setLayoutManager(mLayoutManager);
 
-        // adding inbuilt divider line
         recyclerView.addItemDecoration(new DividerItemDecoration(this, LinearLayoutManager.VERTICAL));
 
-        // adding custom divider line with padding 16dp
-        // recyclerView.addItemDecoration(new MyDividerItemDecoration(this, LinearLayoutManager.HORIZONTAL, 16));
         recyclerView.setItemAnimator(new DefaultItemAnimator());
 
-        recyclerView.setAdapter(mAdapter);
+        recyclerView.setAdapter(mAdapter);*/
 
         // row click listener
-        recyclerView.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), recyclerView, new RecyclerTouchListener.ClickListener() {
+        /*llista.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), llista, new RecyclerTouchListener.ClickListener() {
             @Override
             public void onClick(View view, int position) {
-                Mobile mobile = mobileList.get(position);
+                Mobile mobile = Mobiles.get //.get(position);
                 //Toast.makeText(getApplicationContext(), mobile.getNom() + " is selected!", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(MainActivity.this,DetailMobile.class);
                 intent.putExtra("nom", mobile.getNom());
@@ -78,15 +101,96 @@ public class MainActivity extends AppCompatActivity {
             public void onLongClick(View view, int position) {
 
             }
-        }));
+        }));*/
 
-        prepareMobileData();
+        //prepareMobileData();
+    }
+    /*
+ * Torna a executar la consulta i a enllaçar les dades
+ */
+    void refreshData() {
+        titulars = titularsConv.getAll();
+        adapter = new TitularsAdapter(this, titulars);
+        llista.setAdapter(adapter);
+        if(titulars.getCount() == 0) {
+            lblNoData.setVisibility(lblNoData.VISIBLE);
+            llista.setVisibility(llista.INVISIBLE);
+        }
+        else {
+            lblNoData.setVisibility(lblNoData.INVISIBLE);
+            llista.setVisibility(llista.VISIBLE);
+        }
     }
 
     /**
+     * Crea el menú d'opcions de l'aplicació
+     */
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    /**
+     * Respon a l'event d'haver escollit una opció del menú de l'app
+     */
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.mnuAdd:
+                Intent i = new Intent(this, NouTitularActivity.class);
+                startActivityForResult(i, ADD_TITULAR);
+                return true;
+            case R.id.mnuSortir :
+                this.finish();
+                return true;
+        }
+        return false;
+    }
+
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+        // si s'ha tancat l'activitat ADD_TITULAR i ha anat bé
+        if( requestCode == ADD_TITULAR && resultCode == RESULT_OK) {
+            refreshData();
+        }
+    }
+
+    /**
+     * Crea el menú contextual
+     */
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.contextual, menu);
+    }
+
+    /**
+     * Respon a l'event d'haver escollit una opció del menú contextual
+     */
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo)item.getMenuInfo();
+
+        switch (item.getItemId()) {
+            case R.id.mnuVeureDades:
+                // mostrar les dades de l'element escollit
+                Toast.makeText(this, adapter.getItem(info.position).getTitol(), Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.mnuEsborrar:
+                // esborrar l'element escollit
+                titularsConv.remove(adapter.getItem(info.position));
+                // actualitzar la llista
+                refreshData();
+                // mostrar missatge
+                Toast.makeText(this, "S'ha esborrat el titular!", Toast.LENGTH_LONG).show();
+                return true;
+            default: break;
+        }
+        return false;
+    }
+    /**
      * Prepares sample data to provide data set to adapter
      */
-    private void prepareMobileData(){
+    /*private void prepareMobileData(){
         Mobile mobile = new Mobile("iPhone X", "Apple","iPhone X","12-11-2017", "5,8", "64", "Unknow", "12", "iOS 11", "1159€", "https://images.apple.com/es/iphone/compare/images/overview/compare_iphonex_silver_large.jpg");
         mobileList.add(mobile);
         mobile = new Mobile("iPhone 8", "Apple","iPhone 8","12-11-2017", "4,7", "64", "Unknow", "12", "iOS 11", "809€", "https://images.apple.com/es/iphone/compare/images/overview/compare_iphone8_silver_large.jpg");
@@ -103,7 +207,7 @@ public class MainActivity extends AppCompatActivity {
         // notify adapter about data set changes
         // so that it will render the list with new data
         mAdapter.notifyDataSetChanged();
-    }
+    }*/
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
